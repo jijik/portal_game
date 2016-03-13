c8f4c5bef91975387f75c78f69dac999e6a98d81
jijik <=>
2016-03-13 13:39:45 +0100

    init

---
+++ Source/PortalGame/PortalGameGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PortalGame.h"
+#include "PortalGameGameMode.h"
+
+
+
+
---
+++ Source/PortalGame/PortalGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PortalGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTALGAME_API APortalGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
